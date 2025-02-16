0765fa2bdbb85760d74b7647cb7d73c6a264c9f8
DavidJoao <davidsandoval596@gmail.com>
2025-02-15 17:00:08 -0800

    CREATED: Initial page

---
+++ src/app/pages/plan/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-zinc-900 flex flex-col items-center justify-center min-h-screen p-3'>
+        
+    </div>
+  )
+}
+
+export default page
